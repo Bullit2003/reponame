d12f35785404bc6e5179f61d441aabb7730a5674
robsphone <robsphone@iinet.net.au>
2019-11-15 11:49:09 +1100

    Added Factorial Calculation.

--- reponame.py
+++ reponame.py
@@ -1,1 +1,8 @@
-print("Testing a repository using HTTP URL")
+print("Testing a repository using HTTP URL. Let us calculate factorials.")
+factNum = input("Please Enter a number: ")
+convertedNum = int(factNum)
+answer = 1
+for x in range(convertedNum):
+    x = x + 1
+    answer = answer * x
+print(answer)
